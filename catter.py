6f0ec5a2679a6fd7c9ac8e8d96d708210e1d4bc0
Roland Mechler <rmechler@gmail.com>
2016-08-27 21:29:35 -0700

    cat line by line

--- catter.py
+++ catter.py
@@ -1,7 +1,13 @@
 
+with open('md5s.txt') as md5_file:
+    md5s = md5_file.readlines()
+
 with open('big2.txt', 'w') as f:
-    with open("md5s.txt") as md5s:
-        for md5 in md5s.readlines():
-            print(md5.strip())
-            with open('chunks/' + md5.strip()) as chunk:
-                f.write(chunk.read())
+    for md5 in md5s:
+        print(md5.strip())
+        with open('chunks/' + md5.strip()) as chunk:
+            for line in chunk.readlines():
+                f.write(line)
+
+with open('md5s.txt', 'w') as md5_file:
+    md5_file.writelines(md5s)
