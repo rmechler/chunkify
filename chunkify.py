3c7db6d196f97429be15fed16a8d44d9f8b20ab2
Roland Mechler <rmechler@gmail.com>
2016-09-05 11:00:50 -0700

    added test diff capabilities

--- chunkify.py
+++ chunkify.py
@@ -1,10 +1,56 @@
 
 import subprocess
+import random
 import hashlib
 import os
 
 CHUNK_SIZE = 100000
 MD5_LIST_FILE = 'md5s.txt'
+
+def randint(end):
+    """
+    """
+    return random.randint(0, end-1)
+
+def make_test_file(filename, linesize=1000, numlines=1000, chunksize=CHUNK_SIZE):
+    """
+    """
+    with open(filename, "w") as f:
+        for x in xrange(1, numlines+1):
+            f.write(str(x).zfill(linesize-1) + '\n')
+
+def make_diff_file(old, new, adds=1, deletes=0, changes=0, per_change_limit=1):
+    """
+    """
+    with open(old) as f:
+        lines = f.readlines()
+
+    num_lines = len(lines)
+
+    for x in xrange(adds):
+        index = randint(num_lines)
+        for y in xrange(randint(per_change_limit)+1):
+            newline = 'blah\n'
+            lines.insert(index, newline)
+
+    for x in xrange(deletes):
+        index = randint(num_lines)
+        for y in xrange(randint(per_change_limit)+1):
+            lines.pop(index)
+
+    for x in xrange(changes):
+        index = randint(num_lines)
+        for y in xrange(randint(per_change_limit)+1):
+            lines.pop(index)
+        for y in xrange(randint(per_change_limit)+1):
+            newline = 'blah\n'
+            lines.insert(index, newline)
+
+        # newline = 'blah\n'
+        # lines[randint(num_lines)] = newline
+
+    with open(new, 'w') as f:
+        f.writelines(lines)
 
 def diff(file1, file2):
     """
@@ -15,7 +61,6 @@ def diff(file1, file2):
 
     return diffs
 
-
 def write_chunk(content, md5s_file):
     """
     """
@@ -23,7 +68,6 @@ def write_chunk(content, md5s_file):
     with open("chunks/" + md5, 'w') as chunk_file:
         chunk_file.write(content)
     md5s_file.write(md5 + '\n')
-
 
 def get_chunk_list():
     """
@@ -35,6 +79,29 @@ def get_chunk_list():
 
     return md5s, sizes
 
+def get_chunk_md5s():
+    """
+    """
+    with open(MD5_LIST_FILE) as md5_file:
+        return [md5.strip() for md5 in md5_file.readlines()]
+
+def get_chunk_sizes(md5s):
+    """
+    """
+    return [os.path.getsize("chunks/" + md5)for md5 in md5s]
+
+def assemble_chunks_to_file(filename, chunksdir='chunks'):
+    """
+    """
+    with open(filename, 'w') as f:
+        for md5 in get_chunk_md5s():
+            print(md5.strip())
+            with open(os.path.join(chunksdir, md5.strip())) as chunk:
+                for line in chunk.readlines():
+                    f.write(line)
+
+    # with open(MD5_LIST_FILE, 'w') as md5_file:
+    #     md5_file.writelines(md5s)
 
 def make_chunks(filename):
     """
